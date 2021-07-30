6118f7462cc83db187f4a26f9d534bd618611e43
imchanyang <cksdid636@naver.com>
2021-07-31 02:26:45 +0900

    second commit

--- Tensorflow0.py
+++ Tensorflow0.py
@@ -1,4 +1,20 @@
 import tensorflow as tf
 
-Tensor = tf.constant(3)
-print(Tensor)
+Tensor1 = tf.constant([3, 4, 5])
+Tensor2 = tf.constant([6, 7, 8])
+
+Tensor3 = tf.constant([[1, 2], [3, 4]])
+Tensor4 = tf.constant([[1, 2], [3, 4]])
+
+print(tf.matmul(Tensor3, Tensor4))
+
+Tensor5 = tf.zeros([2, 2, 3])
+print(Tensor5)
+
+
+W = tf.Variable(1.0)
+print(W)
+
+W.assign(2.0)
+print(W.numpy())
+
